5de921ac8ea759df2a6eb7b50f4a64d9cc3c3ac8
karthick77 <karthick77@users.noreply.github.com>
2023-06-04 12:42:32 +0530

    networthInCR to totalNetworthInCR

--- DataWorks/src/Actors/Kamal.java
+++ DataWorks/src/Actors/Kamal.java
@@ -3,7 +3,7 @@ package Actors;
 public class Kamal {
 
 	public String starName = "Kamal Hasan";
-	public double networthInCR = 600.00;
+	public double totalNetworthInCR = 600.00;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Kamal sir is nothing but cinema, for me cinema is nothing but Kamal sir");
