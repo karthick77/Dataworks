708306c10bc761b31758ea000bd83ca5b1e59ad4
karthick77 <mail2bkarthick@gmail.com>
2023-06-04 10:35:09 +0530

    first commit

---
+++ DataWorks/src/Actors/Kamal.java
@@ -0,0 +1,10 @@
+package Actors;
+
+public class Kamal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Kamal sir is nothing but cinema, for me cinema is nothing but Kamal sir");
+	}
+
+}
---
+++ DataWorks/src/Actors/Rajini.java
@@ -0,0 +1,10 @@
+package Actors;
+
+public class Rajini {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("One and Only Super-Star");
+	}
+
+}
