d7ca9a7f37b4359a37a97a06bc31bb94af41ac8a
karthick77 <karthick77@users.noreply.github.com>
2023-06-04 13:13:50 +0530

    city

--- DataWorks/src/Actors/Rajini.java
+++ DataWorks/src/Actors/Rajini.java
@@ -4,7 +4,7 @@ public class Rajini {
 
 	public String starName = "Rajinikanth";
 	public String networthInCR = "800";
-	public String address;
+	public String city;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("One and Only Super-Star");
